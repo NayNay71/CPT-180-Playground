53e9b3d053e3358408cb337c7012aea2237b42f1
NayNay71 <brownna@my.wccc.edu>
2016-01-31 12:19:34 -0500

    Added some comments and code to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+// We are not using the std namespace in our code
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+//Create and initialize a variable for miles
+
+int miles_traveled = 2000
